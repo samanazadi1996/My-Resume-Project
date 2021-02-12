08b46ea4d7020638affe263ca649491e22354a77
Saman Azadi <60857846+samanazadi1996@users.noreply.github.com>
2021-02-12 14:16:03 +0330

    Delete Infrastructure.DomainModel directory

--- Infrastructure.DomainModel/Identity/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Infrastructure.DomainModel.Common;
-using Microsoft.AspNetCore.Identity;
-
-namespace Infrastructure.DomainModel.Identity
-{
-    public class ApplicationRole : IdentityRole<int>, IEntity
-    {
-    }
-}
